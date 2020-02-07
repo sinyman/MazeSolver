9842a3de5cb05d571bbb0f8be92a0ea212d05374
Simon Nyman <nyman.simon@hotmail.com>
2020-02-07 15:39:25 +0200

    Added initial test file

---
+++ MazeSolver/src/test/java/MazeTest.java
@@ -0,0 +1,15 @@
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import inputOutput.MazeFactory;
+/**
+ * Class that contains the tests for the Maze I/O.
+ * @author cherry
+ */
+public class MazeTest {
+    
+    	@Test
+	public void testNothing() {
+            assertTrue(true);
+	}
+}
